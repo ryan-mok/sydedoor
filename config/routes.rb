a6434d0e4aa46718b621a2355ff2eeb78c760484
Ryan Mok <ryan-mok@users.noreply.github.com>
2018-05-31 20:18:12 -0400

    Add a controller for static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'application#hello'
+  root 'static_pages#home'
+  get 'static_pages/home'
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
   resources :users
