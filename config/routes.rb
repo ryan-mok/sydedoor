a7cfff373cb1f29ec19c229fa62213333043ee21
Ryan Mok <ryan-mok@users.noreply.github.com>
2018-05-31 20:41:00 -0400

    Replace root URL with static home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-<<<<<<< HEAD
   root 'static_pages#home'
-  get 'static_pages/home'
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
   resources :users
-
-=======
-  get 'static_pages/home'
-
-  root 'application#hello'
->>>>>>> Use dynamic page titles in layout
 end
