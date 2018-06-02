cb7c432a9e3640768de259549b7c6bd6ec822751
Annie <a.zhangishere@gmail.com>
2018-06-01 19:47:02 -0700

    Users controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'application#hello'
+  get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
+  resources :users
+
 end
