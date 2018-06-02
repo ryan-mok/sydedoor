0062c4a9bafaadbc756c037b8e13943943b32790
Eason Gao <easongao1998@gmail.com>
2018-06-01 23:20:20 -0400

    added some restrictions to model

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,6 @@
 class Company < ApplicationRecord
+  validates :name,  presence: true
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
