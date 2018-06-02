f2306be68bfcc56fecccacd0d9a3cc48ca075002
Eason Gao <easongao1998@gmail.com>
2018-06-02 17:32:32 -0400

    THE TESTS WORK YESSSS

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,3 @@
 class Company < ApplicationRecord
   validates :name,  presence: true
-  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
-  has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
 end
