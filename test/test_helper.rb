64fdda6dff817092c715c4c00d6794ff34c6c5c8
Ryan Mok <ryan-mok@users.noreply.github.com>
2018-06-06 21:43:35 -0400

    Fix minitest-reporters issue

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,5 @@
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
-require 'minitest/reporters'
-Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
