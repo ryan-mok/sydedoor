4443770b88e217926e512c1c5aeeaecf6a52f958
Ryan Mok <ryan-mok@users.noreply.github.com>
2018-06-06 20:48:41 -0400

    Add migration to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180602030849) do
+ActiveRecord::Schema.define(version: 20180606220626) do
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 20180602030849) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_companies_on_name", unique: true
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "user_id"
+    t.string "company_id"
+    t.text "description"
+    t.integer "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
