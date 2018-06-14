c67f7ece5eaacf95de85e4406b0f219a45f0ad3c
Eason Gao <easongao1998@gmail.com>
2018-06-13 21:33:13 -0400

    updated companies controller, routes, and show html

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,2 +1,25 @@
 class CompaniesController < ApplicationController
+
+  def index
+    @company = Company.all
+    render html: "TODO: company display page"
+  end
+
+  def new
+    @company = Company.new
+  end
+
+  def create
+    @company = Company.new(@company)    # Not the final implementation!
+    if @company.save
+      redirect_to @company
+    else
+      render 'new'
+    end
+  end
+
+  def company_params
+    params.require(:company).permit(:name, :description, :website)
+  end
+
 end
