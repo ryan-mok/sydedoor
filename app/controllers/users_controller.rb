0bdbd5e7033a0f078bbbc0b3c346d9230e7ba78b
Annie Zhang <t-anzhan@microsoft.com>
2018-06-30 00:32:35 -0700

    early setup of edit / delete actions for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
   def index
     render html: 'TODO: user display page'
   end
@@ -22,9 +23,32 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      @user.save!
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
+
+  # Confirms a logged in user and flashes a notice to log in
+  def logged_in_user
+    unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+    end
   end
 end
