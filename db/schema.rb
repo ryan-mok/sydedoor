039f963ddb37f54615011eb8b77c56dd78562d8b
Annie Zhang <t-anzhan@microsoft.com>
2018-07-06 13:52:52 -0700

    add advanced login with remember me option

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180616010617) do
+ActiveRecord::Schema.define(version: 20180630073726) do
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20180616010617) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -35,4 +35,18 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get root_path
     assert flash.empty?
   end
+
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_not_empty cookies['remember_token']
+    assert_equal FILL_IN, assigns(:user).FILL_IN
+  end
+
+  test "login without remembering" do
+    # Log in to set the cookie.
+    log_in_as(@user, remember_me: '1')
+    # Log in again and verify that the cookie is deleted.
+    log_in_as(@user, remember_me: '0')
+    assert_empty cookies['remember_token']
+  end
 end
