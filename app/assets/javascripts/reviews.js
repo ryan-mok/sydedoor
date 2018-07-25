b952fa92debdf2c9aef8c16fbdc0c5d417e19b04
Ryan Mok <ryan-mok@users.noreply.github.com>
2018-07-24 23:04:22 -0400

    Fix associations for form submission

--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $('#review_company').autocomplete({ minLength : 0 }).focus(function() {
+    $('#company').autocomplete({ minLength : 0 }).focus(function() {
         $(this).autocomplete("search");
     });
 });
