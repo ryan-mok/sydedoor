02b4cdc1f6480c05255cacca998b3938d86f2845
Ryan Mok <ryan-mok@users.noreply.github.com>
2018-08-15 23:00:01 -0400

    Fix fixtures to have proper assocation between all the objects and refactor tests

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,6 @@
 class ReviewsController < ApplicationController
-  before_action :logged_in_user , only: [:index, :create, :new]
-  #before_action :correct_user,   only: [:edit, :update, :destroy]
+  before_action :logged_in_user, only: %i[index create new]
+  # before_action :correct_user,   only: [:edit, :update, :destroy]
   autocomplete :company, :name, full: true, limit: 1000
 
   def index
@@ -52,20 +52,20 @@ class ReviewsController < ApplicationController
         .require(:review)
         .permit(:user_id, :company_id, :rating, :job_title, :term, :year, :salary, :location, :description)
     end
-  def logged_in_user
-    return if logged_in?
 
-    store_location
-    flash[:danger] = "Please log in."
-    redirect_to login_url
-  end
+    def logged_in_user
+      return if logged_in?
 
-  def correct_user
-    #Grabbing wrong user
-    @user = User.find(params[:id])
-    flash[:danger] = @user
-    #Figure out way to only display correct options like edit and delete to appropriate user, need to edit 'show.html.erb'
-    redirect_to(root_url) unless current_user?(@user)
-  end
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
 
+    def correct_user
+      # Grabbing wrong user
+      @user = User.find(params[:id])
+      flash[:danger] = @user
+      # Figure out way to only display correct options like edit and delete to appropriate user, need to edit 'show.html.erb'
+      redirect_to(root_url) unless current_user?(@user)
+    end
 end
